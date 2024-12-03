b80a6ff6247198d1f554acdfe6d15d2cbc5072d2
Ashish Gupta <109547698+Ashish-k-Gupta@users.noreply.github.com>
2024-12-03 23:25:45 +0530

    just added button component, yet to start working in that component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet,RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PipeComponent } from "./pipe/pipe.component";
 import { TemplateFormValidationComponent } from "./template-form-validation/template-form-validation.component";
+import { InputOutputReuseableComponent } from "./input-output-reuseable/input-output-reuseable.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // imports: [RouterOutlet, EmployeeListComponent, AddEmployeeComponent, DataBindingComponent],
-  imports: [RouterOutlet, CommonModule, RouterModule, PipeComponent, TemplateFormValidationComponent],
+  imports: [RouterOutlet, CommonModule, RouterModule, TemplateFormValidationComponent, InputOutputReuseableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
