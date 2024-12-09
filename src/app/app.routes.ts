776ce725de969ee8676faf74eafd1a4f351a276f
Ashish-k-gupta-1 <theashukumar007@gmail.com>
2024-12-09 16:47:11 +0530

    fixed route file, they are supposed to be setup in way that the default page should be login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,38 +7,62 @@ import { SwitchCaseComponent } from './switch-case/switch-case.component';
 import { CrudAPISComponent } from './crud-apis/crud-apis.component';
 import { PipeComponent } from './pipe/pipe.component';
 import { InputOutputReuseableComponent } from './input-output-reuseable/input-output-reuseable.component';
+// import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginUsingAPIComponent } from './login-using-api/login-using-api.component';
 
 export const routes: Routes = [
     {
-        path: 'add-emp',
-        component: AddEmployeeComponent
+        path: '',
+        redirectTo: 'loginapi',
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'loginapi',
+        component: LoginUsingAPIComponent
     },
     {
-        path: 'emp-list',
-        component: EmployeeListComponent
-    },
-    {
-        path: 'data-binding',
-        component: DataBindingComponent
-    },
-    {
-        path: 'routing-examples',
-        component: RoutingComponent
-    },
-    {
-        path: 'swtich-case',
-        component: SwitchCaseComponent
-    },
-    {
-        path: 'crud-apis',
-        component: CrudAPISComponent
-    },
-    {
-        path: 'pipe',
-        component: PipeComponent
-    },
-    {
-        path: 'inputOutput',
-        component: InputOutputReuseableComponent
-    },
+        path: '',
+        component: LayoutComponent,
+        children: [
+
+
+            {
+                path: 'add-emp',
+                component: AddEmployeeComponent
+            },
+            {
+                path: 'emp-list',
+                component: EmployeeListComponent
+            },
+            {
+                path: 'data-binding',
+                component: DataBindingComponent
+            },
+            {
+                path: 'routing-examples',
+                component: RoutingComponent
+            },
+            {
+                path: 'swtich-case',
+                component: SwitchCaseComponent
+            },
+            {
+                path: 'crud-apis',
+                component: CrudAPISComponent
+            },
+            {
+                path: 'pipe',
+                component: PipeComponent
+            },
+            {
+                path: 'inputOutput',
+                component: InputOutputReuseableComponent
+            }
+        ]
+
+            
+        }
+    
 ];
